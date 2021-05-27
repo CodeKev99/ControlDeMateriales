6904837e105a640cb775daa5a190008462305314
William <wrodas2530@gmail.com>
2021-05-27 12:17:05 -0600

    validar canpo proveedor en producto

--- src/vista/PInventario.java
+++ src/vista/PInventario.java
@@ -63,7 +63,6 @@ public class PInventario extends javax.swing.JPanel {
         txtVenta = new javax.swing.JTextField();
         jLabel11 = new javax.swing.JLabel();
         txtCantidad = new javax.swing.JTextField();
-        txtprove = new javax.swing.JTextField();
         jScrollPane2 = new javax.swing.JScrollPane();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbmaterial = new javax.swing.JTable();
@@ -232,8 +231,6 @@ public class PInventario extends javax.swing.JPanel {
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/vista/iconos/icons8_search_30px_3.png"))); // NOI18N
 
-        conbox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -248,9 +245,7 @@ public class PInventario extends javax.swing.JPanel {
                                     .addGap(34, 34, 34)
                                     .addComponent(txtmas, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(lname, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(conbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addComponent(lname, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 482, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(124, 124, 124)
@@ -271,15 +266,18 @@ public class PInventario extends javax.swing.JPanel {
                                     .addComponent(jLabel8)
                                     .addComponent(jLabel11)
                                     .addComponent(jLabel9))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtNombre)
-                                    .addComponent(txtCompra, javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(txtCantidad)
-                                    .addComponent(txtVenta)
-                                    .addComponent(txtprove, javax.swing.GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
-                                    .addComponent(cbmedida, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(cbtipo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(18, 18, 18)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(txtNombre)
+                                            .addComponent(txtCompra, javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(txtCantidad)
+                                            .addComponent(txtVenta)
+                                            .addComponent(cbmedida, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(cbtipo, 0, 127, Short.MAX_VALUE)))
+                                    .addComponent(conbox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addGap(191, 191, 191))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGap(31, 31, 31)
@@ -317,7 +315,7 @@ public class PInventario extends javax.swing.JPanel {
                         .addGap(13, 13, 13)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel9)
-                            .addComponent(txtprove, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(conbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(24, 24, 24)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel4)
@@ -346,8 +344,7 @@ public class PInventario extends javax.swing.JPanel {
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(txtmas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(lname)
-                            .addComponent(conbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lname))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(btnEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -445,10 +442,46 @@ public class PInventario extends javax.swing.JPanel {
 
     private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
 
+            String Proveedor = conbox.getSelectedItem().toString();        
+         int id=0;
+        PreparedStatement pss;
+        ResultSet rss;
+        ResultSetMetaData rsmdd;
+      
+        try {
+            
+            Connection con;
+            con = Conexion.getConexion();
+            pss = con.prepareStatement("SELECT idProve FROM proveedor WHERE empresa=?");
+            pss.setString(1, Proveedor);
+            rss = pss.executeQuery();
+            rsmdd = rss.getMetaData();
+            
+                        while (rss.next()) {
+               
+            id = rss.getInt(1);
+
+            }
+        
+              
+            
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.toString());
+
+        }
+        
+        
+        
+        
+        
+        
+        
         String Nombre = txtNombre.getText();
         String Tipo = cbtipo.getSelectedItem().toString();
 
-        int Proveedor = Integer.parseInt(txtprove.getText());
+       
+       
         String medida = cbmedida.getSelectedItem().toString();
         Double compra = Double.parseDouble(txtCompra.getText());
         Double venta = Double.parseDouble(txtVenta.getText());
@@ -457,7 +490,7 @@ public class PInventario extends javax.swing.JPanel {
         try {
             Connection con = Conexion.getConexion();
             PreparedStatement ps = con.prepareStatement("INSERT INTO materiales (idProve,nombre,tipo,medida,cantidad,precioCompra,PrecioVenta) VALUES (?,?,?,?,?,?,?)");
-            ps.setInt(1, Proveedor);
+            ps.setInt(1, id);
             ps.setString(2, Nombre);
             ps.setString(3, Tipo);
             ps.setString(4, medida);
@@ -551,7 +584,7 @@ public class PInventario extends javax.swing.JPanel {
             
             Connection con;
             con = Conexion.getConexion();
-            ps = con.prepareStatement("SELECT nombre FROM proveedor");
+            ps = con.prepareStatement("SELECT empresa FROM proveedor");
             rs = ps.executeQuery();
             rsmd = rs.getMetaData();
             
@@ -654,6 +687,5 @@ public class PInventario extends javax.swing.JPanel {
     private javax.swing.JTextField txtNombre;
     private javax.swing.JTextField txtVenta;
     private javax.swing.JTextField txtmas;
-    private javax.swing.JTextField txtprove;
     // End of variables declaration//GEN-END:variables
 }
